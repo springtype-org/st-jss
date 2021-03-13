1f31f62aaf4ca52096e22a674525c1e1496a1caa
Michael Mannseicher <m.mansi1@gmx.net>
2021-03-14 00:08:26 +0100

    fix: css style

--- src/function/getCssAsText.ts
+++ src/function/getCssAsText.ts
@@ -1,5 +1,12 @@
+import { camelToKebabCase } from './camelToKebabCase';
+
 export const getCssAsText = (key: string, value: any) => {
   const helperEl: any = document.createElement('div');
   helperEl.style[key] = value;
-  return helperEl.style.cssText;
+  const documentStyle = helperEl.style.cssText;
+  // dont work with :before content: ""
+  if (documentStyle !== '') {
+    return documentStyle;
+  }
+  return `${camelToKebabCase(key)}: ${value};`;
 };
