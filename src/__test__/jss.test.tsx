6ee2de25186139f7b6bcdf6196071c89a2e548d3
Michael Mannseicher <m.mansi1@gmx.net>
2021-03-14 01:12:40 +0100

    chore: add tests and ci

--- src/__test__/jss.test.tsx
+++ src/__test__/jss.test.tsx
@@ -1,7 +1,28 @@
 import { jss } from '../jss';
+import { camelToKebabCase } from '../function/camelToKebabCase';
+import { getCssAsText } from '../function/getCssAsText';
+import { joinClassNames } from '../function/joinClassNames';
 
 describe('jss', () => {
   it('is defined', () => {
     expect(jss).toBeDefined();
+  });
+});
+describe('camelToKebabCase', () => {
+  it('is equal', () => {
+    expect(camelToKebabCase('backgroundColor')).toEqual('background-color');
+    expect(camelToKebabCase('background-color')).toEqual('background-color');
+  });
+});
+describe('getCssAsText', () => {
+  it('is equal', () => {
+    expect(getCssAsText('content', '""')).toEqual('content: "";');
+    expect(getCssAsText('borderColor', '#fff')).toEqual('border-color: #fff;');
+  });
+});
+describe('joinClassNames', () => {
+  it('is equal', () => {
+    expect(joinClassNames(['st', 'header', 3])).toEqual('st-header-3');
+    expect(joinClassNames(['st', 'container', 0])).toEqual('st-container-0');
   });
 });
