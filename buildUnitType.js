5ed947c9745e0f20ca21c43d7eb116c4fce9571c
Michael Mannseicher <m.mansi1@gmx.net>
2021-09-15 01:33:14 +0200

    fix: generation step

--- buildUnitType.js
+++ buildUnitType.js
@@ -1,20 +1,25 @@
-const json = require('mdn-data/css/properties.json')
+const json = require('mdn-data/css/properties.json');
 const fs = require('fs');
 const path = require('path');
 
-const result = ['/* this code is generated */',
-  'export const cssLengthRawKeys: { [cssProperty: string]: boolean } = {'
+const result = [
+  '/* this code is generated */',
+  'export const cssLengthRawKeys: { [cssProperty: string]: boolean } = {',
 ];
-Object.entries(json).filter(([key, value]) => !key.startsWith('-') && value.syntax.indexOf('<length>') > -1)
+Object.entries(json)
+  .filter(
+    ([key, value]) =>
+      !key.startsWith('-') &&
+      (value.syntax.indexOf('<length>') > -1 || value.syntax.indexOf('<length-percentage>') > -1),
+  )
   .forEach(([key, value]) => {
-    result.push(`  // syntax: ${value.syntax.trim()}`)
+    result.push(`  // syntax: ${value.syntax.trim()}`);
     if (key.indexOf('-') > -1) {
-      result.push(`  '${key}': true,`)
+      result.push(`  '${key}': true,`);
     } else {
-      result.push(`  ${key}: true,`)
+      result.push(`  ${key}: true,`);
     }
   });
 result.push('};\n');
 
-fs.writeFileSync(path.join(__dirname, 'src', 'cssLengthRawKeys.ts'), result.join('\n'))
-
+fs.writeFileSync(path.join(__dirname, 'src', 'cssLengthRawKeys.ts'), result.join('\n'));
