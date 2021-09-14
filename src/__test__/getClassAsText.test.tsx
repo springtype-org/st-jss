90cf3ae6baa34b786983db9333fdbfe402c2b879
Michael Mannseicher <m.mansi1@gmx.net>
2021-09-15 01:34:56 +0200

    add: tests

--- src/__test__/getClassAsText.test.tsx
+++ src/__test__/getClassAsText.test.tsx
@@ -6,6 +6,8 @@ describe('getCssAsText', () => {
     expect(getCssAsText('borderColor', '#fff', 'px')).toEqual('border-color: #fff;');
     expect(getCssAsText('height', '100%', 'px')).toEqual('height: 100%;');
     expect(getCssAsText('height', 1, 'px')).toEqual('height: 1px;');
+    expect(getCssAsText('fontSize', 22, 'px')).toEqual('font-size: 22px;');
+    expect(getCssAsText('marginTop', 20, 'px')).toEqual('margin-top: 20px;');
     expect(getCssAsText('margin-top', 20, 'px')).toEqual('margin-top: 20px;');
   });
 });
